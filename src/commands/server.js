74a681671488e72aa1d0a081384845b3142caba3
300aj11 <300aj11@gmail.com>
2023-01-29 16:17:32 +0530

    EphemeralResponses

--- src/commands/server.js
+++ src/commands/server.js
@@ -7,6 +7,9 @@ module.exports = {
 		
 	async execute(interaction) {
 		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		await interaction.reply({
+			content: `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`,
+			emphemeral:true
+		});
 	},
 };
